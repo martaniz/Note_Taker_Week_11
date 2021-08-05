53db92958de7a325d3f9e1f2832eab6998acd234
marta nizynska <78944173+martaniz@users.noreply.github.com>
2021-08-05 19:09:56 -0400

    final

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,11 +2,12 @@ const router = require("express").Router();
 const notes = require('../../db/db.json');
 
 router.get("/notes", function(req, res) {
-    console.log("hit the getrout")
-    console.log(notes)
-    return res.json(notes)
+  return res.json(notes);
+      res.send(["notes", 404]); 
+
 //  res.send(notes);
-})
+});
+
 
 // router.post("/notes", function(req, res){
 //  save the note to db.json
@@ -23,6 +24,29 @@ router.post('/notes', (req, res) => {
     res.json(notes);
   }
 });
+
+
+// DELETE /api/users/1
+router.delete('/:id', (req, res) => {
+  User.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+
 
 
 
--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.use(express.static('public'));
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
+
+
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 })
